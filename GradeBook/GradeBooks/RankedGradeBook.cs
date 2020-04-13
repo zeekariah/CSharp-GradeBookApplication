f3e86ea2a7c4ce41d29a598bcc324caa1cbf85b1
zachariahscovill <zachariahscovill@gmail.com>
2020-04-13 11:55:25 -0600

    Added override of GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,25 +19,18 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
 
-            var twentyPercent = (int)Math.Ceiling(Students.Count * 0.2);
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-            
-            if (averageGrade >= grades[twentyPercent])
-            {
+
+            if (averageGrade >= grades[threshold - 1])
                 return 'A';
-            }
-            if (averageGrade >= grades[twentyPercent * 2 - 1])
-            {
+            if (averageGrade >= grades[(threshold * 2) - 1])
                 return 'B';
-            }
-            else if (averageGrade >= grades[twentyPercent * 3 - 1])
-            {
+            if (averageGrade >= grades[(threshold * 3) - 1])
                 return 'C';
-            }
-            else if (averageGrade >= grades[twentyPercent * 4 - 1])
-            {
+            if (averageGrade >= grades[(threshold * 4) - 1])
                 return 'D';
-            }
+            return 'F';
 
             return 'F';
         }
