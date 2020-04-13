f3336fe373afa275f503910a67f0dbf31b582758
zachariahscovill <zachariahscovill@gmail.com>
2020-04-13 12:34:10 -0600

    Step 4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -20,6 +20,7 @@ namespace GradeBook.GradeBooks
             }
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var threshold2 = Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
             if (averageGrade >= grades[threshold - 1])
@@ -31,8 +32,28 @@ namespace GradeBook.GradeBooks
             if (averageGrade >= grades[(threshold * 4) - 1])
                 return 'D';
             return 'F';
+        }
 
-            return 'F';
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
